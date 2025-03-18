c605a6b99387c2720e0a9c97210d6f7a33e136ee
jinxudong <18751241086@163.com>
2025-03-18 19:41:45 +0800

    更改路径

--- vue.config.js
+++ vue.config.js
@@ -4,6 +4,7 @@ function resolve(dir) {
 }
 // https://cli.vuejs.org/zh/guide/webpack.html#%E7%AE%80%E5%8D%95%E7%9A%84%E9%85%8D%E7%BD%AE%E6%96%B9%E5%BC%8F
 module.exports = {
+  publicPath: '/vue-admin-pro/',
   devServer: {
     // 配置反向代理
     proxy: {
